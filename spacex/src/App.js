e022c1adc2e8eb06060dfebb3d37b0f3ad80dc21
Jordan Morales <jordan.morales@gmail.com>
2019-12-13 12:58:22 -0800

    initial setup

---
+++ spacex/src/App.js
@@ -0,0 +1,29 @@
+// =============================
+// DEPENDENCIES
+// =============================
+// packages
+import React from 'react';
+
+// components
+
+// =============================
+// COMPONENT CLASS
+// =============================
+class App extends React.Component {
+
+//// ==============
+//// RENDER
+//// ==============
+  render(){
+    return(
+      <div>
+      This is app.js
+      </div>
+    )
+  }
+}
+
+// =============================
+// EXPORT
+// =============================
+export default App;
