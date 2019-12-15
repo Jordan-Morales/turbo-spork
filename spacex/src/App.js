99ba23813e69afbb5bfc5346a758e93e0906bbf4
Jordan Morales <jordan.morales@gmail.com>
2019-12-15 10:19:50 -0800

    test

--- spacex/src/App.js
+++ spacex/src/App.js
@@ -15,6 +15,7 @@ import Main from './component/Main'
 // ExternalAPI-URL Definer
 let apiUrl = 'https://api.spacexdata.com/v3/launches/';
 let intApiUrl = 'http://turbo-spork-app.herokuapp.com/api/'
+let test = 'test'
 
 class App extends React.Component {
   constructor(props){
