b017ab0b2d5c457dc2d9dc81358827c76efa571c
KB-Z <zdonner179@gmail.com>
2019-12-16 17:34:05 -0500

    updated Create Table with new header names

--- api/seed.sql
+++ api/seed.sql
@@ -1,4 +1,4 @@
-CREATE TABLE launches (id SERIAL, likes INT, flight_number INT, notes VARCHAR(256));
+CREATE TABLE launches (id SERIAL, likes INT, flight_number INT, mission_name VARCHAR(64), site_name_long VARCHAR(128), launch_date_local VARCHAR(32), notes VARCHAR(256));
 
 
 INSERT INTO launches (
