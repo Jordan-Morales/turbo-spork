a156556983cca7a98d4798e3d44ded64707408c3
Jordan Morales <jordan.morales@gmail.com>
2019-12-17 11:12:00 -0800

    organized

--- api/controllers/launch.php
+++ api/controllers/launch.php
@@ -4,21 +4,27 @@ header('Content-Type: application/json');
 
 if ($_REQUEST['action'] === 'index') {
   echo json_encode(Launches::all());
-  } elseif ($_REQUEST['action'] === 'post') {
-    $request_body = file_get_contents('php://input');
-    $body_object = json_decode($request_body);
-    $new_launch = new Launch(null, $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
-    $all_launches = Launches::create($new_launch);
-    echo json_encode($all_launches);
-    } else if ($_REQUEST['action'] === 'update'){
-      $request_body = file_get_contents('php://input');
-      $body_object = json_decode($request_body);
-      $updated_launch = new Launch($_REQUEST['id'], $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
-      $all_launches = Launches::update($updated_launch);
-      echo json_encode($all_launches);
-      } else if ($_REQUEST['action'] === 'delete') {
-        $all_launches = Launches::delete($_REQUEST['id']);
-        echo json_encode($all_launches);
-        }
+  }
+
+elseif ($_REQUEST['action'] === 'post') {
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $new_launch = new Launch(null, $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
+  $all_launches = Launches::create($new_launch);
+  echo json_encode($all_launches);
+  }
+
+else if ($_REQUEST['action'] === 'update'){
+  $request_body = file_get_contents('php://input');
+  $body_object = json_decode($request_body);
+  $updated_launch = new Launch($_REQUEST['id'], $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
+  $all_launches = Launches::update($updated_launch);
+  echo json_encode($all_launches);
+  }
+
+else if ($_REQUEST['action'] === 'delete') {
+  $all_launches = Launches::delete($_REQUEST['id']);
+  echo json_encode($all_launches);
+  }
 
 ?>
