e6fb9c019722bf448c7fe5d8edb86211707c1e3b
KB-Z <zdonner179@gmail.com>
2019-12-17 13:01:55 -0500

    troublehsooting update issue new launch was not Capitalized.

--- api/controllers/launch.php
+++ api/controllers/launch.php
@@ -6,13 +6,13 @@ if ($_REQUEST['action'] === 'index') {
   } elseif ($_REQUEST['action'] === 'post') {
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
-    $new_launch = new launch(null, $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
+    $new_launch = new Launch(null, $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
     $all_launches = Launches::create($new_launch);
     echo json_encode($all_launches);
     } else if ($_REQUEST['action'] === 'update'){
       $request_body = file_get_contents('php://input');
       $body_object = json_decode($request_body);
-      $updated_launch = new launch($_REQUEST['id'], $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
+      $updated_launch = new Launch($_REQUEST['id'], $body_object->likes, $body_object->flight_number, $body_object->mission_name, $body_object->site_name_long, $body_object->launch_date_local, $body_object->notes);
       $all_launches = Launches::update($updated_launch);
       echo json_encode($all_launches);
       } else if ($_REQUEST['action'] === 'delete') {
